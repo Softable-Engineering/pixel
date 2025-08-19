a284c5692185e81a47d56f6867fa6fd6f0901d8d
mbrito <mauricio@softable.com.br>
2025-08-19 19:26:04 -0300

    feat: finally actions to preset column

--- src/components/toolkit/Calendar/components/CalendarModal/hooks/useCalendar/utils/shortcutGroup/getRelativePreset.ts
+++ src/components/toolkit/Calendar/components/CalendarModal/hooks/useCalendar/utils/shortcutGroup/getRelativePreset.ts
@@ -1,9 +1,41 @@
 // Utils
+import { getTodayPreset } from '../shortcuts'
 import { getRelative } from './relative/getRelative'
 
 // Types
 import { TypeRelative } from './relative/types'
-import type { ShortcutGroup } from '@components/toolkit/Calendar/types'
+import type {
+  BuildContext,
+  DateFilterValue,
+  Shortcut,
+  ShortcutGroup
+} from '@components/toolkit/Calendar/types'
+
+function getYesterdayPreset(): Shortcut {
+  return {
+    id: 'yesterday',
+    label: 'Ontem',
+    build: (_: BuildContext): DateFilterValue => {
+      return {
+        op: 'equals',
+        at: { type: 'token', token: 'yesterday' }
+      }
+    }
+  }
+}
+
+function getTomorrowPreset(): Shortcut {
+  return {
+    id: 'tomorrow',
+    label: 'Amanha',
+    build: (_: BuildContext): DateFilterValue => {
+      return {
+        op: 'equals',
+        at: { type: 'token', token: 'tomorrow' }
+      }
+    }
+  }
+}
 
 export function getRelativePreset(): ShortcutGroup {
   return {
@@ -11,6 +43,9 @@ export function getRelativePreset(): ShortcutGroup {
     label: 'Relativo',
     items: [
       [
+        getTodayPreset(),
+        getYesterdayPreset(),
+        getTomorrowPreset(),
         getRelative(TypeRelative.MONTH),
         getRelative(TypeRelative.BIMESTER),
         getRelative(TypeRelative.TRIMESTER),
