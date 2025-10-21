37a8d9d94500724f88af679618c5d053e7ad6159
mbrito <mauricio@softable.com.br>
2025-10-21 08:39:24 -0300

    feat: add other methods in formula input

--- src/components/tables/TableView/modals/FormulaModal/utils/functionsDescriptors.ts
+++ src/components/tables/TableView/modals/FormulaModal/utils/functionsDescriptors.ts
@@ -24,4 +24,106 @@ export const AVG: FunctionDescriptor = {
   }
 }
 
-export const FUNCTIONS: FunctionDescriptor[] = [SUM, AVG]
+export const COUNT: FunctionDescriptor = {
+  value: 'count',
+  displayName: 'COUNT()',
+  params: ['...number'],
+  description: 'Média dos argumentos',
+  evaluateForPreview: (...args: any[]) => {
+    return '-'
+  }
+}
+
+export const MAX: FunctionDescriptor = {
+  value: 'max',
+  displayName: 'MAX()',
+  params: ['...number'],
+  description: 'Média dos argumentos',
+  evaluateForPreview: (...args: any[]) => {
+    return '-'
+  }
+}
+
+export const MIN: FunctionDescriptor = {
+  value: 'min',
+  displayName: 'MAX()',
+  params: ['...number'],
+  description: 'Média dos argumentos',
+  evaluateForPreview: (...args: any[]) => {
+    return '-'
+  }
+}
+
+export const YEAR_FROM: FunctionDescriptor = {
+  value: 'year_from',
+  displayName: 'YEAR_FROM()',
+  params: ['...number'],
+  description: 'Média dos argumentos',
+  evaluateForPreview: (...args: any[]) => {
+    return '-'
+  }
+}
+
+export const MONTH_FROM: FunctionDescriptor = {
+  value: 'month_from',
+  displayName: 'MONTH_FROM()',
+  params: ['...number'],
+  description: 'Média dos argumentos',
+  evaluateForPreview: (...args: any[]) => {
+    return '-'
+  }
+}
+
+export const DAY_FROM: FunctionDescriptor = {
+  value: 'day_from',
+  displayName: 'DAY_FROM()',
+  params: ['...number'],
+  description: 'Média dos argumentos',
+  evaluateForPreview: (...args: any[]) => {
+    return '-'
+  }
+}
+
+export const DAY_OF_WEEK: FunctionDescriptor = {
+  value: 'day_of_week',
+  displayName: 'DAY_OF_WEEK()',
+  params: ['...number'],
+  description: 'Média dos argumentos',
+  evaluateForPreview: (...args: any[]) => {
+    return '-'
+  }
+}
+
+export const HOUR_FROM: FunctionDescriptor = {
+  value: 'hour_from',
+  displayName: 'HOUR_FROM()',
+  params: ['...number'],
+  description: 'Média dos argumentos',
+  evaluateForPreview: (...args: any[]) => {
+    return '-'
+  }
+}
+
+export const YEAR_MONTH_FROM: FunctionDescriptor = {
+  value: 'year_month_from',
+  displayName: 'YEAR_MONTH_FROM()',
+  params: ['...number'],
+  description: 'Média dos argumentos',
+  evaluateForPreview: (...args: any[]) => {
+    return '-'
+  }
+}
+
+export const FUNCTIONS: FunctionDescriptor[] = [
+  SUM,
+  AVG,
+  COUNT,
+  MAX,
+  MIN,
+  YEAR_FROM,
+  MONTH_FROM,
+  DAY_FROM,
+  DAY_OF_WEEK,
+  HOUR_FROM,
+  YEAR_MONTH_FROM
+]
